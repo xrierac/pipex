f9f233e0d16b039d9b925716d460afab06bc959e
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-02-12 12:30:57 +0200

    Add child and parent functions

--- pipex.c
+++ pipex.c
@@ -6,35 +6,54 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 13:50:40 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/02/10 13:42:21 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/02/12 12:12:17 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
+#include "pipex.h"
+#include "libft/libft.h"
 
-extern char **environ;
+extern char	**environ;
+
+void	left_cmd(char **argv, char **environ, int pipefd[])
+{
+	int	infile;
+
+	infile = open(argv[1], O_RDONLY, 00444);
+	dup2(infile, STDIN_FILENO);
+	close(infile);
+	dup2(pipefd[1], STDOUT_FILENO);
+	execute(argv[2], environ);
+}
+
+void	right_cmd(char **argv, char **environ, int pipefd[])
+{
+	int	outfile;
+
+	outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 00222);
+	dup2(outfile, STDOUT_FILENO);
+	close(outfile);
+	dup2(pipefd[0], STDIN_FILENO);
+	execute(argv[3], environ);
+}
+
 int	main(int argc, char *argv[])
 {
 	int		pipefd[2];
 	pid_t	cpid;
 
+	if (argc != 5)
+		return (0);
+	pipe(pipefd);
 	cpid = fork();
 	if (cpid == 0)
-
+		left_cmd(argv, environ, pipefd);
 	else if (cpid == -1)
-
+		perror("Error");
 	else
-
-//	if (argc != 5)
-//		perror("Wrong number of arguments");
-	if (argc == 5)
 	{
-		if (access(argv[1], F_OK) == -1)
-			perror("Error");
-		execve(
-
-	}
-//	cpid = fork();
+		wait(&cpid);
+		right_cmd(argv, environ, pipefd);
+	}	
 	return (0);
 }
