fcac894e1cf2ede05975f748f6273e72276dd2c9
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-02-12 12:31:14 +0200

    Add function declarations

--- pipex.h
+++ pipex.h
@@ -6,11 +6,18 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 11:22:27 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/02/10 14:24:18 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/02/12 12:12:10 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 # include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
 
+void	left_cmd(char **argv, char **environ, int pipefd[]);
+void	right_cmd(char **argv, char **environ, int pipefd[]);
+char	*get_path(char **environ, char *cmd);
+void	execute(char *str, char **environ);
+#endif
