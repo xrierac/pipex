5d218ba6c362ed8f26f53808710ed51c46de317e
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-02-20 14:38:48 +0200

    Modify quoting functions to include double quoting

--- utils.c
+++ utils.c
@@ -6,35 +6,38 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 13:59:33 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/02/20 12:31:33 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/02/20 14:35:07 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include "libft/libft.h"
 
-static int	check_quotes(char *str, int c)
+//This functions checks for a quoted string and if it exists it replaces empty 
+//spaces with quotes so ft_split can operate normally 
+static char	check_quotes(char *str)
 {
-	int	i;
-	int	p;
+	int		i;
+	char	c;
 
 	i = -1;
-	p = 0;
+	c = ' ';
 	while (str[++i])
 	{
-		if (str[i] == c && ft_strchr(str + i + 1, c) != 0)
+		if (str[i] == 39 || str[i] == 34)
 		{
-			str[i] = ' ';
-			p = c;
-			while (str[++i] != c)
+			c = str[i];
+			if (ft_strchr(str + i + 1, c))
 			{
-				if (str[i] == ' ')
-					str[i] = c;
+				str[i] = ' ';
+				while (str[++i] != c)
+					if (str[i] == ' ')
+						str[i] = c;
+				str[i] = ' ';
 			}
-			str[i] = ' ';
 		}
 	}
-	return (p);
+	return (c);
 }
 
 static char	**get_cmd(char *str)
@@ -42,20 +45,20 @@ static char	**get_cmd(char *str)
 	char	**cmd;
 	int		i;
 	int		j;
+	char	c;
 
-	check_quotes(str, 39);
-	check_quotes(str, 34);
+	c = check_quotes(str);
 	cmd = ft_split(str, ' ');
 	if (cmd == 0)
 		return (0);
-	if (ft_strchr(str, 39) != 0 || ft_strchr(str, 34) != 0)
+	if (c != ' ')
 	{
 		i = -1;
 		while (cmd[++i])
 		{
 			j = -1;
 			while (cmd[i][++j])
-				if (cmd[i][j] == 39 || cmd[i][j] == 34)
+				if (cmd[i][j] == c)
 					cmd[i][j] = ' ';
 		}
 	}
@@ -76,7 +79,7 @@ char	*get_path(char **environ, char *cmd)
 	arr = ft_split(environ[i] + 5, ':');
 	cmd = ft_strjoin("/", cmd);
 	if (arr == 0 || cmd == 0)
-		error_exit("");
+		error_exit("", 127);
 	i = -1;
 	while (arr[++i])
 	{
@@ -99,16 +102,19 @@ void	execute(char *str, char **environ)
 
 	cmd = get_cmd(str);
 	if (cmd == 0)
-		error_exit("");
-	path = get_path(environ, cmd[0]);
+		error_exit("", 127);
+	if (access(cmd[0], F_OK) != 0)
+		path = get_path(environ, cmd[0]);
+	else
+		path = cmd[0];
 	if (path == 0)
 	{
 		ft_putstr_fd("pipex: ", 2);
 		ft_putstr_fd(cmd[0], 2);
 		ft_putstr_fd(" : command not found\n", 2);
 		ft_free_array(cmd);
-		exit(EXIT_FAILURE);
+		exit(127);
 	}
 	if (execve(path, cmd, environ) == -1)
-		error_exit(cmd[0]);
+		error_exit(cmd[0], 127);
 }
