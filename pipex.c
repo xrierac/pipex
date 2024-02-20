5b4a2717aa0be5da9fabc49d793087e75d5a5f62
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-02-20 14:37:43 +0200

    Modify error_exit function

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 13:50:40 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/02/20 11:42:07 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/02/20 14:34:33 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 
 extern char	**environ;
 
-void	error_exit(char *str)
+void	error_exit(char *str, int status)
 {
 	ft_putstr_fd("pipex: ", 2);
 	perror(str);
-	exit(EXIT_FAILURE);
+	exit(status);
 }
 
 void	left_cmd(char **argv, char **environ, int fd[])
@@ -28,9 +28,9 @@ void	left_cmd(char **argv, char **environ, int fd[])
 
 	infile = open(argv[1], O_RDONLY, 0444);
 	if (infile == -1)
-		error_exit(argv[1]);
+		error_exit(argv[1], 127);
 	if (dup2(infile, STDIN_FILENO) == -1 || dup2(fd[1], STDOUT_FILENO) == -1)
-		error_exit("");
+		error_exit("", 127);
 	close(fd[0]);
 	close(fd[1]);
 	execute(argv[2], environ);
@@ -42,9 +42,9 @@ void	right_cmd(char **argv, char **environ, int fd[])
 
 	outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (outfile == -1)
-		error_exit(argv[4]);
+		error_exit(argv[4], 127);
 	if (dup2(outfile, STDOUT_FILENO) == -1 || dup2(fd[0], STDIN_FILENO) == -1)
-		error_exit("");
+		error_exit("", 127);
 	close(fd[1]);
 	close(fd[0]);
 	execute(argv[3], environ);
@@ -57,21 +57,21 @@ int	parent_process(char **environ, char **argv)
 	pid_t	cpid2;
 
 	if (pipe(pipefd) < 0)
-		error_exit("");
+		error_exit("", 127);
 	cpid1 = fork();
 	if (cpid1 < 0)
-		error_exit("");
+		error_exit("", 127);
 	if (cpid1 == 0)
 		left_cmd(argv, environ, pipefd);
 	cpid2 = fork();
 	if (cpid2 < 0)
-		error_exit("");
+		error_exit("", 127);
 	if (cpid2 == 0)
 		right_cmd(argv, environ, pipefd);
 	close(pipefd[0]);
 	close(pipefd[1]);
 	if (waitpid(cpid1, NULL, 0) == -1 || waitpid(cpid2, NULL, 0) == -1)
-		error_exit("");
+		error_exit("", 127);
 	return (0);
 }
 
